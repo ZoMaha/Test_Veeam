edc57492f688947162a37a12b2a0142afe06af19
Мария Касицына <m.kasitsina@yandex.ru>
2021-07-06 21:55:23 +0300

    _

--- monitor/Program.cs
+++ monitor/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.ComponentModel;
 using System.Threading;
+using System.IO;
 
 namespace monitor
 {
@@ -20,27 +21,55 @@ namespace monitor
             }
             catch
             {
+               
                 processName = "notepad";
-                lifetime = 5;
+                lifetime = 2;
                 checkFrequency = 1;
+                Console.WriteLine("You didn't provide input parameters");
             }
 
             try
             {
-
-                while (true == true)
+                DateTime now = DateTime.Now;
+                string filename = @"D:\logMonitor\monitorlog" + now.ToShortDateString() + "_" + now.Hour + "." + now.Minute + "." + now.Second + ".txt";
+                //File logfile = File.CreateText(filename);
+                using (StreamWriter logfile = File.CreateText(filename))
                 {
-                    foreach (var process in Process.GetProcessesByName(processName))
+                    string addString = "The \"monitor\" was start at " + now.ToString();
+                    logfile.WriteLine(addString);
+                    Console.WriteLine(addString);
+                    while (Console.ReadKey().Key != ConsoleKey.Escape)
                     {
-                        DateTime date = process.StartTime;
-                        DateTime now = DateTime.Now;
-                        TimeSpan timeSpan = now - date;
-                        if (timeSpan.TotalMinutes >= lifetime)
+                        addString = "\"Monitor\" started searching for processes at " + DateTime.Now.ToString();
+                        logfile.WriteLine(addString);
+                        Console.WriteLine(addString);
+                        foreach (var process in Process.GetProcessesByName(processName))
                         {
-                            process.Kill();
+                            addString = "Process named \"" + processName + "\" was found at " + DateTime.Now.ToString();
+                            logfile.WriteLine(addString);
+                            Console.WriteLine(addString);
+
+                            DateTime date = process.StartTime;
+                            now = DateTime.Now;
+                            TimeSpan timeSpan = now - date;
+                            addString = "The lifetime of process with name \"" + processName + "\" is " + timeSpan.TotalMinutes + " minutes";
+                            logfile.WriteLine(addString);
+                            Console.WriteLine(addString);
+                            if (timeSpan.TotalMinutes >= lifetime)
+                            {
+                                process.Kill();
+                                addString = "Process named \"" + processName + "\" killed on " + DateTime.Now.ToString();
+                                logfile.WriteLine(addString);
+                                Console.WriteLine(addString);
+                            }
+                            Console.WriteLine();
                         }
                         Thread.Sleep(checkFrequency * 60000);
                     }
+                    addString = "\"Monitor\" endind its work on " + DateTime.Now.ToString();
+                    logfile.WriteLine(addString);
+                    Console.WriteLine(addString);
+                    Environment.Exit(0);
                 }
             }
             catch
