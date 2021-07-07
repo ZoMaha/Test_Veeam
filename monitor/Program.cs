9ba9762736abfb11c485cb2091f5f69641c5fbe3
Мария Касицына <m.kasitsina@yandex.ru>
2021-07-07 14:28:00 +0300

    Last version

--- monitor/Program.cs
+++ monitor/Program.cs
@@ -3,44 +3,58 @@ using System.Diagnostics;
 using System.ComponentModel;
 using System.Threading;
 using System.IO;
+using System.Text;
 
 namespace monitor
 {
     class Program
     {
+        private static bool stop = false;
+        public static string filename = @"D:\logMonitor\monitorlog";
+
         static void Main(string[] args)
         {
+
             string processName;
             int lifetime;
             int checkFrequency;
-            try
-            {
-                processName = args[0];
-                lifetime = Int32.Parse(args[1]);
-                checkFrequency = Int32.Parse(args[2]);
-            }
-            catch
-            {
-               
-                processName = "notepad";
-                lifetime = 2;
-                checkFrequency = 1;
-                Console.WriteLine("You didn't provide input parameters");
-            }
 
             try
             {
+                try
+                {
+                    processName = args[0];
+                    lifetime = Int32.Parse(args[1]);
+                    checkFrequency = Int32.Parse(args[2]);
+                }
+                catch
+                {
+
+                    processName = "notepad";
+                    lifetime = 2;
+                    checkFrequency = 1;
+                    Console.WriteLine("You didn't provide input parameters. Logfile didn't create.");
+                    Environment.Exit(0);
+                }
+
+                System.Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
                 DateTime now = DateTime.Now;
-                string filename = @"D:\logMonitor\monitorlog" + now.ToShortDateString() + "_" + now.Hour + "." + now.Minute + "." + now.Second + ".txt";
-                //File logfile = File.CreateText(filename);
+                filename += now.ToShortDateString() + "_" + now.Hour + "." + now.Minute + "." + now.Second + ".txt";
+
                 using (StreamWriter logfile = File.CreateText(filename))
                 {
                     string addString = "The \"monitor\" was start at " + now.ToString();
                     logfile.WriteLine(addString);
                     Console.WriteLine(addString);
-                    while (Console.ReadKey().Key != ConsoleKey.Escape)
+                }
+
+
+                while (!stop)
+                {
+
+                    using (StreamWriter logfile = new StreamWriter(filename, true, Encoding.UTF8))
                     {
-                        addString = "\"Monitor\" started searching for processes at " + DateTime.Now.ToString();
+                        string addString = "\"Monitor\" started searching for processes at " + DateTime.Now.ToString();
                         logfile.WriteLine(addString);
                         Console.WriteLine(addString);
                         foreach (var process in Process.GetProcessesByName(processName))
@@ -64,20 +78,28 @@ namespace monitor
                             }
                             Console.WriteLine();
                         }
-                        Thread.Sleep(checkFrequency * 60000);
                     }
-                    addString = "\"Monitor\" endind its work on " + DateTime.Now.ToString();
-                    logfile.WriteLine(addString);
-                    Console.WriteLine(addString);
-                    Environment.Exit(0);
+                    Thread.Sleep(checkFrequency * 60000);
                 }
             }
             catch
             {
                 Console.WriteLine("ERROR");
             }
-            
+
+        }
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            stop = true;
+            using (StreamWriter logfile = new StreamWriter(filename, true, Encoding.UTF8))
+            {
+                string addString = "\"Monitor\" endind its work on " + DateTime.Now.ToString();
+                logfile.WriteLine(addString);
+                Console.WriteLine(addString);
+            }
+            e.Cancel = false;
         }
     }
+
 }
 
